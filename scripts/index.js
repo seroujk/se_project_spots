6a36cdcf042d58532874a6005c9e2084e24885f6
SeroujK <seroujguidanian@gmail.com>
2025-01-14 11:16:27 -0800

    Added JS

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [ 
+    firstCard ={
+        name: "Val Thorens",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+    },
+    secondCard ={
+        name: "Restaurant terrace",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+    },
+    thirdCard ={
+        name: "An outdoor cafe",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+    },
+    fourthCard ={
+        name: "A very long bridge, over the forest and through the trees",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+    },
+    fifthCard ={
+        name: "Tunnel with morning light",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+    },
+    sixthCard ={
+        name: "Mountain house",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+    },
+   ]
