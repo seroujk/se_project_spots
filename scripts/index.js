0fecc81f2c0be21e2bbc171268179a6d754e3c2e
SeroujK <seroujguidanian@gmail.com>
2025-01-27 20:16:18 -0800

    Added Edit Profile Functionality and Generated Cards From Template

--- scripts/index.js
+++ scripts/index.js
@@ -25,13 +25,82 @@ const initialCards = [
   }),
 ];
 
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  //Declare and choose the template that was made for the cards and mae it cardTemplate
+  const cardTemplate = document.querySelector("#card__template").content;
+
+  //Declare the actual card element that we want to add to the DOM
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  //Declare a variable for the card title
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  //Declare a varuabe for the card Image
+  const cardImage = cardElement.querySelector(".card__image");
+  
+
+  //Populate the element with the current data from the list
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+
+  return cardElement;
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  const card = getCardElement(initialCards[i]);
+  cardsList.append(card);
+}
+
 let modal = document.querySelector("#edit-modal");
 let editBtn = document.querySelector(".profile__edit-btn");
 let closeBtn = document.querySelector(".modal__close-btn");
+let submitBtn = document.querySelector(".modal__submit-btn");
 
+//open the modal
 editBtn.addEventListener("click", function showModal() {
   modal.classList.add("modal_opened");
 });
-closeBtn.addEventListener("click", function showModal() {
+// hide the modal
+closeBtn.addEventListener("click", function hideModal() {
   modal.classList.remove("modal_opened");
 });
+// submit the modal and close it
+submitBtn.addEventListener("click", function closeModal() {
+  modal.classList.remove("modal_opened");
+});
+
+const profileFormElement = document.querySelector(".modal__form");
+
+const nameInput = profileFormElement.querySelector("#name");
+const jobInput = profileFormElement.querySelector("#description");
+
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__description");
+
+//Insert the current profile name value into the form's input field
+nameInput.value = profileNameElement.textContent;
+//Insert the current profile job value into the form's input field
+jobInput.value = profileJobElement.textContent;
+
+// The form submission handler. Note that its name
+// starts with a verb and concisely describes what it does.
+function handleProfileFormSubmit(evt) {
+  // Prevent default browser behavior, see explanation below.
+  evt.preventDefault();
+
+  // TODO: Get the values of each form field from the value property
+  // of the corresponding input element.
+  let nameInputValue = nameInput.value;
+  let jobInputValue = jobInput.value;
+  // TODO: Then insert these new values into the textContent property of the
+  // corresponding profile elements.
+  profileNameElement.textContent = nameInputValue;
+  profileJobElement.textContent = jobInputValue;
+  // TODO: Close the modal.
+}
+
+// Connect the handler to the form, so it will watch for the submit event.
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
